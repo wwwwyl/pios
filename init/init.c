e2566b0092bfa2240beaa9c46ccc24030eca3088
wyl <1513078461@qq.com>
2022-05-01 20:08:06 +0800

    t(exc.c): Add test excption

--- init/init.c
+++ init/init.c
@@ -1,10 +1,22 @@
 #include "printf.h"
 #include "pmap.h"
 
+void exc_test(){
+    printf("init.c:\texc_test() is called\n");
+    printf("there shoule be an Exception below\n");
+    unsigned int r;
+    // generate a Data Abort with a bad address access
+    r=*((volatile unsigned int*)0xFFFFFFFFFF000000);
+    // lol, shut up gcc
+    r++;
+    panic("init.c:\tend of exc_test() reached!");
+}
+
 void aarch64_init(){
     printf("init.c:\taarch64_init() is called\n");
 
 
+    exc_test();
 
 
     panic("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
--- init/main.c
+++ init/main.c
@@ -2,12 +2,13 @@
 #include "printf.h"
 #include "pmap.h"
 
-void main(){
+int main(){
     uart_init();
 
 	printf("main.c:\tmain is start ...\n");
 
 	aarch64_init();
+    
 	panic("main is over is error!");
 
 	return 0;
